373022f09fb318f50cf18919b2bd2181d0a78921
LeliaP <lelia-polit@orange.fr>
2020-10-20 10:44:05 +0200

    Update functions_CHIPIN.R

--- R/functions_CHIPIN.R
+++ R/functions_CHIPIN.R
@@ -1,22 +1,20 @@
-# library(stringr)
-# library(rtracklayer)
-# library(ggplot2)
-# library(matrixStats)
-# library(viridis)
-# library(preprocessCore)
-# library(tidyr)
-# library(plotrix)
-# library(rlist)
-# library(RColorBrewer)
-# library(tiger)
-# library(pracma)
-# library(GenomicFeatures)
-# library(dplyr)
-# library(gridExtra)
-# library(biomaRt)
-# library(rminer)
-# library(ggpubr)
+#Copywrite Lélia Polit, 2020
 
+# >>> SOURCE LICENSE >>>
+#   This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation (www.fsf.org); either version 2 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# A copy of the GNU General Public License is available at
+# http://www.fsf.org/licensing/licenses
+# 
+# >>> END OF LICENSE >>>
 
 ###
 '%ni%' <- Negate('%in%')
